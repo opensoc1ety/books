df020d5f3b5f1932c998b7b5318c198c4887a2fa
Sujauddin <61101752+sujaudd1n@users.noreply.github.com>
2022-10-27 18:16:38 +0530

    Implement poem API

---
+++ assets/scripts/script.js
@@ -0,0 +1,30 @@
+document.querySelector('.btn').addEventListener('click', function(){
+    fetch('https://www.poemist.com/api/v1/randompoems')
+    .then(response => response.json())
+    .then(data => {display_poems(data)})
+})
+
+function display_poems(poems)
+{
+    for (let i = 0; i < poems.length; ++i)
+        document.querySelector('.poems').appendChild(get_poem(poems[i]));
+}
+
+function get_poem(p)
+{
+    let article = document.createElement("article");
+    article.setAttribute('class', 'poems__poem');
+    let poet = document.createElement("p");
+    poet.setAttribute('class', 'poem__poet')
+    let poem = document.createElement("p");
+    poem.setAttribute('class', 'poem__content')
+    let title = document.createElement("p");
+    title.setAttribute('class', 'poem__title')
+    poem.innerHTML = p["content"].replaceAll("\n", "<span class=\"linebreak\"></span>");
+    poet.textContent = "By " + p["poet"]["name"];
+    title.textContent = p["title"];
+    article.appendChild(title);
+    article.appendChild(poem);
+    article.appendChild(poet);
+    return article;
+}
